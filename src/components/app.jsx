53c4f27c6692a812c96b01b825db7ad7396c86c6
Kunihiko MIYOSHI <miyoshik@gmail.com>
2017-10-18 21:13:55 +0900

    replace original component to react-leaflet

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,21 @@
 import React from "react"
-import Map from "./map"
+
+import { Map, Polygon, Popup, TileLayer } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
 
 export default class App extends React.Component {
   render() {
-    return <div><Map /></div>
+    const center = [43.065617, 141.348541]
+    const osmUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    const osmAttrib =
+      '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+
+    return (
+      <div>
+        <Map center={center} zoom={7} className="sesekimap">
+          <TileLayer attribution={osmAttrib} url={osmUrl} opacity={0.2} />
+        </Map>
+      </div>
+    )
   }
 }
