e239e3d6b06281a473bdc5a1757dcbc475363139
Kunihiko Miyoshi <contact@colspan.net>
2017-12-01 23:25:15 +0900

    split appheader

---
+++ src/components/appHeader.jsx
@@ -0,0 +1,56 @@
+import React from "react"
+import { Button, Dropdown, Header, Icon } from "semantic-ui-react"
+
+import actions from "../actions"
+import { prefectureDef } from "../helpers/params"
+
+export default class AppHeader extends React.Component {
+  render() {
+    const prefectureOptions = prefectureDef.map(x => {
+      return { key: x.id, value: x.id, text: x.prefecture_jp }
+    })
+    const columnOptions = this.props.geoStatData
+      ? this.props.geoStatData.csvKeys.slice(1, -1).map((x, i) => {
+        return { key: i, value: i, text: x }
+      })
+      : []
+
+    const headerElem = (
+      <div id="header">
+        <Header as="h2">
+          <Icon name="map outline" />
+          <Header.Content>
+            Seseki Viewer
+          <Header.Subheader className="subtitle">Visualize Geo Statistical Data</Header.Subheader>
+          </Header.Content>
+        </Header>
+        <div id="prefecture_selector">
+          <Dropdown
+            placeholder="Select Prefecture"
+            multiple
+            fluid
+            selection
+            options={prefectureOptions}
+            value={this.props.areas.map(x => x.id)}
+            onChange={(e, x) => this.props.dispatch({ type: actions.AREA_CHANGE, data: { areas: x.value } })}
+          />
+        </div>
+        <div id="toolbox">
+          <Button content="Open CSV" icon="file excel outline" labelPosition="left" />
+          <Button content="Edit" icon="edit" labelPosition="left" />
+        </div>
+        <div id="column-selector">
+          <Dropdown
+            placeholder="Select column"
+            fluid
+            selection
+            options={columnOptions}
+            value={this.props.geoStatisticalDataColumn}
+            onChange={(e, x) => this.props.dispatch({ type: actions.GEOSTATISTICALDATA_CHANGE_COLUMN, data: { column: x.value } })}
+          />
+        </div>
+      </div>
+    )
+    return headerElem
+  }
+}
--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -1,17 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { connect } from "react-redux"
-import { parse } from "query-string"
 
-import { Button, Dropdown, Header, Icon } from "semantic-ui-react"
 import "semantic-ui-css/semantic.min.css"
 
 import actions from "../actions"
 
 import HeatMap from "../components/heatmap"
+import AppHeader from "../components/appHeader"
 
 import GeoStatisticalData from "../helpers/geoStatisticalData"
-import { prefectureDef } from "../helpers/params"
 
 function mapStateToProps(state, ownProps) {
   const props = Object.assign({}, ownProps, state)
@@ -34,54 +32,11 @@ class App extends React.Component {
       )
     }
 
-    const mapElem = (<HeatMap {...Object.assign({}, this.props.seseki, { geoStatData }) } />)
+    const childProps = Object.assign({}, this.props.seseki, { geoStatData, dispatch: this.props.dispatch })
 
+    const headerElem = (<AppHeader {...childProps} />)
+    const mapElem = (<HeatMap {...childProps } />)
     const rankingElem = <div id="ranking">ranking</div>
-
-    const prefectureOptions = prefectureDef.map(x => {
-      return { key: x.id, value: x.id, text: x.prefecture_jp }
-    })
-    const columnOptions = geoStatData
-      ? geoStatData.csvKeys.slice(1, -1).map((x, i) => {
-        return { key: i, value: i, text: x }
-      })
-      : []
-    const headerElem = (
-      <div id="header">
-        <Header as="h2">
-          <Icon name="map outline" />
-          <Header.Content>
-            Seseki Viewer
-            <Header.Subheader className="subtitle">Visualize Geo Statistical Data</Header.Subheader>
-          </Header.Content>
-        </Header>
-        <div id="prefecture_selector">
-          <Dropdown
-            placeholder="Select Prefecture"
-            multiple
-            fluid
-            selection
-            options={prefectureOptions}
-            value={this.props.seseki.areas.map(x => x.id)}
-            onChange={(e, x) => this.props.dispatch({ type: actions.AREA_CHANGE, data: { areas: x.value } })}
-          />
-        </div>
-        <div id="toolbox">
-          <Button content="Open CSV" icon="file excel outline" labelPosition="left" />
-          <Button content="Edit" icon="edit" labelPosition="left" />
-        </div>
-        <div id="column-selector">
-          <Dropdown
-            placeholder="Select column"
-            fluid
-            selection
-            options={columnOptions}
-            value={this.props.seseki.geoStatisticalDataColumn}
-            onChange={(e, x) => this.props.dispatch({ type: actions.GEOSTATISTICALDATA_CHANGE_COLUMN, data: { column: x.value } })}
-          />
-        </div>
-      </div>
-    )
 
     return (
       <div id="container">
