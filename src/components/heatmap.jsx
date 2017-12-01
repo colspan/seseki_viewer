b3fa11f82643d8171eea2cb174e716b284f005dc
Kunihiko Miyoshi <contact@colspan.net>
2017-12-01 21:14:52 +0900

    split heatmap component

--- src/components/heatmap.jsx
+++ src/components/heatmap.jsx
@@ -1,6 +1,12 @@
-import { GeoJSON } from "react-leaflet"
+import React from "react"
+import { Map, GeoJSON, Popup, TileLayer } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
+import { geoCentroid } from "d3-geo"
 
-export default class HeatMap extends GeoJSON {
+
+import GeoStatisticalData from "../helpers/geoStatisticalData"
+
+class MyGeoJSON extends GeoJSON {
   updateLeafletElement(fromProps, toProps) {
     const geoJsonLayer = this.leafletElement
     geoJsonLayer.getLayers().forEach(x => {
@@ -10,3 +16,119 @@ export default class HeatMap extends GeoJSON {
     super.updateLeafletElement(fromProps, toProps)
   }
 }
+
+export default class Heatmap extends React.Component {
+  render() {
+    /* heatmap layer */
+    const heatmapAttrib =
+      '&copy; <a href="http://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N03.html">国土数値情報 行政区域データ</a>, ' +
+      'CC BY NC SA 4.0 <a href="https://github.com/colspan">Miyoshi(@colspan)</a> <a href="https://github.com/colspan/seseki_viewer">Seseki</a>'
+    let featureStyle = {
+      color: "#222",
+      weight: 0.3,
+      opacity: 0.6,
+      fillOpacity: 0.6,
+      fillColor: "#ffffff"
+    }
+    let eachFeature = (d, l) => {
+      l.bindTooltip(d.name)
+      l.on({
+        mouseover: e => { },
+        mouseout: e => { },
+        click: e => { }
+      })
+    }
+    /* データがあったらイベントを付与する */
+    let geoStatData = null
+    if (this.props.geoJson && this.props.geoStatisticalData) {
+      /* データをバインドする */
+      geoStatData = new GeoStatisticalData(
+        this.props.geoStatisticalData,
+        this.props.idMap,
+        this.props.communes
+      )
+      const targetColumn = geoStatData.getByColumnName(geoStatData.csvKeys[this.props.geoStatisticalDataColumn + 1])
+      const idMap = this.props.idMap
+      featureStyle = d => {
+        return {
+          color: "#222",
+          weight: 0.3,
+          opacity: 0.6,
+          fillOpacity: 0.6,
+          fillColor: targetColumn.colorScale(targetColumn.parsedData[idMap[d.name]])
+        }
+      }
+      eachFeature = (d, layer, parent) => {
+        const communeId = d.communeId
+        const commune_name = d.name
+        const value = targetColumn.format(targetColumn.parsedData[idMap[d.name]])
+        /* binding tooltip */
+        const tooltipElem = document.createElement("span")
+        tooltipElem.innerHTML = `<span class="commune_name">${commune_name}</span><div class="value">${value}</div>`
+        tooltipElem.addEventListener("click", x => {
+          // click({ name: commune_name, communeId: communeId })
+        })
+        layer.bindTooltip(tooltipElem, { className: "layer_tooltip" })
+        layer.on({
+          mouseover: e => {
+            const style = {
+              weight: 5,
+              fillColor: "#dce775",
+              fillOpacity: 0.7
+            }
+            /* 同じ市町村を同時に塗りかえる */
+            if (parent)
+              parent
+                .getLayers()
+                .filter(y => y.feature.communeId === communeId)
+                .forEach(y => {
+                  y.setStyle(style)
+                  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+                    y.bringToFront()
+                  }
+                })
+            e.target.openTooltip()
+          },
+          mouseout: e => {
+            /* 同じ市町村を同時に塗りかえる */
+            if (parent)
+              parent
+                .getLayers()
+                .filter(y => y.feature.communeId === communeId)
+                .forEach(y => {
+                  y.setStyle(featureStyle(y.feature))
+                })
+            e.target.closeTooltip()
+          },
+          click: e => {
+            // TODO
+          }
+        })
+      }
+    }
+    const heatMapElem = this.props.geoJson ? (
+      <MyGeoJSON
+        data={this.props.geoJson}
+        style={featureStyle}
+        onEachFeature={eachFeature}
+        attribution={heatmapAttrib}
+      />
+    ) : null
+
+    /* tile layer */
+    const centroid = this.props.geoJson ? geoCentroid(this.props.geoJson) : [141.348541, 43.065617]
+    const osmUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    const osmAttrib = '&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    const tileLayerElem = <TileLayer attribution={osmAttrib} url={osmUrl} opacity={0.2} />
+
+    const mapElem = (
+      <Map center={[centroid[1], centroid[0]]} zoom={7} className="sesekimap" refs="map">
+        {heatMapElem}
+        {tileLayerElem}
+      </Map>
+    )
+
+    return mapElem
+  }
+}
+
--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -3,16 +3,10 @@ import ReactDOM from "react-dom"
 import { connect } from "react-redux"
 import { parse } from "query-string"
 
-import { Map, Polygon, Popup, TileLayer } from "react-leaflet"
-import "leaflet/dist/leaflet.css"
-import { geoCentroid } from "d3-geo"
-
 import { Button, Dropdown, Header, Icon } from "semantic-ui-react"
 import "semantic-ui-css/semantic.min.css"
 
 import actions from "../actions"
-
-import GeoStatisticalData from "../helpers/geoStatisticalData"
 
 import HeatMap from "../components/heatmap"
 
@@ -29,115 +23,19 @@ class App extends React.Component {
   }
   componentDidUpdate() { }
   render() {
-    /* tile layer */
-    const centroid = this.props.seseki.geoJson ? geoCentroid(this.props.seseki.geoJson) : [141.348541, 43.065617]
-    const osmUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    const osmAttrib = '&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    const tileLayerElem = <TileLayer attribution={osmAttrib} url={osmUrl} opacity={0.2} />
 
-    /* heatmap layer */
-    const heatmapAttrib =
-      '&copy; <a href="http://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N03.html">国土数値情報 行政区域データ</a>, ' +
-      'CC BY NC SA 4.0 <a href="https://github.com/colspan">Miyoshi(@colspan)</a> <a href="https://github.com/colspan/seseki_viewer">Seseki</a>'
-    let featureStyle = {
-      color: "#222",
-      weight: 0.3,
-      opacity: 0.6,
-      fillOpacity: 0.6,
-      fillColor: "#ffffff"
-    }
-    let eachFeature = (d, l) => {
-      l.bindTooltip(d.name)
-      l.on({
-        mouseover: e => { },
-        mouseout: e => { },
-        click: e => { }
-      })
-    }
-    /* データがあったらイベントを付与する */
     let geoStatData = null
-    if (this.props.seseki.geoJson && this.props.seseki.geoStatisticalData) {
+    if (this.props.geoJson && this.props.geoStatisticalData) {
       /* データをバインドする */
       geoStatData = new GeoStatisticalData(
-        this.props.seseki.geoStatisticalData,
-        this.props.seseki.idMap,
-        this.props.seseki.communes
+        this.props.geoStatisticalData,
+        this.props.idMap,
+        this.props.communes
       )
-      const targetColumn = geoStatData.getByColumnName(geoStatData.csvKeys[this.props.seseki.geoStatisticalDataColumn + 1])
-      const idMap = this.props.seseki.idMap
-      featureStyle = d => {
-        return {
-          color: "#222",
-          weight: 0.3,
-          opacity: 0.6,
-          fillOpacity: 0.6,
-          fillColor: targetColumn.colorScale(targetColumn.parsedData[idMap[d.name]])
-        }
-      }
-      eachFeature = (d, layer, parent) => {
-        const communeId = d.communeId
-        const commune_name = d.name
-        const value = targetColumn.format(targetColumn.parsedData[idMap[d.name]])
-        /* binding tooltip */
-        const tooltipElem = document.createElement("span")
-        tooltipElem.innerHTML = `<span class="commune_name">${commune_name}</span><div class="value">${value}</div>`
-        tooltipElem.addEventListener("click", x => {
-          // click({ name: commune_name, communeId: communeId })
-        })
-        layer.bindTooltip(tooltipElem, { className: "layer_tooltip" })
-        layer.on({
-          mouseover: e => {
-            const style = {
-              weight: 5,
-              fillColor: "#dce775",
-              fillOpacity: 0.7
-            }
-            /* 同じ市町村を同時に塗りかえる */
-            if (parent)
-              parent
-                .getLayers()
-                .filter(y => y.feature.communeId === communeId)
-                .forEach(y => {
-                  y.setStyle(style)
-                  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-                    y.bringToFront()
-                  }
-                })
-            e.target.openTooltip()
-          },
-          mouseout: e => {
-            /* 同じ市町村を同時に塗りかえる */
-            if (parent)
-              parent
-                .getLayers()
-                .filter(y => y.feature.communeId === communeId)
-                .forEach(y => {
-                  y.setStyle(featureStyle(y.feature))
-                })
-            e.target.closeTooltip()
-          },
-          click: e => {
-            // TODO
-          }
-        })
-      }
     }
-    const heatMapElem = this.props.seseki.geoJson ? (
-      <HeatMap
-        data={this.props.seseki.geoJson}
-        style={featureStyle}
-        onEachFeature={eachFeature}
-        attribution={heatmapAttrib}
-      />
-    ) : null
 
-    const mapElem = (
-      <Map center={[centroid[1], centroid[0]]} zoom={7} className="sesekimap" refs="map">
-        {heatMapElem}
-        {tileLayerElem}
-      </Map>
-    )
-
+    const mapElem = (<HeatMap {...this.props.seseki} />)
+    
     const rankingElem = <div id="ranking">ranking</div>
 
     const prefectureOptions = prefectureDef.map(x => {
